7630e67c2de1baff2097e6059deb865ebf4862a1
mathur karthik <mathurkarthik@example.com>
2020-07-09 17:10:02 -0500

    create models and add to admin view

--- restaurantapp/admin.py
+++ restaurantapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Restaurant, MenuItem
 
 # Register your models here.
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    # display in change list page
+    list_display = ('name',)
+
+    # columns to display in detail list page
+    fields = ('name', 'menu_item')
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    # display in change list page
+    list_display = ('name', 'price')
+
+    # columns to display in detail list page
+    fields = ('name', 'description', 'price', 'course')
+
--- restaurantapp/models.py
+++ restaurantapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+#Createyourmodelshere.
+class Restaurant(models.Model):
+    """create the restaurant model"""
+
+    name = models.CharField(max_length=155,
+                            null=False)
+    menu_item = models.ForeignKey('MenuItem',
+                                null=True,
+                                blank=True,
+                                on_delete=models.SET_NULL,)
+
+
+class MenuItem(models.Model):
+    """create the menu item model"""
+    name = models.CharField(max_length=80,
+                            null=False)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=12,decimal_places=2)
+    course = models.CharField(max_length=30)
