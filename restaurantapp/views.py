11557a920ac9111d66e9cd7ba706e8282b7f9195
mathur karthik <mathurkarthik@example.com>
2020-07-10 08:10:47 -0500

    add restaurant list view

--- restaurantapp/views.py
+++ restaurantapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Restaurant
+
+
+def index(request):
+    restaurant_list = Restaurant.objects.order_by('name')
+    context = {'restaurant_list': restaurant_list}
+
+    return render(request, 'restaurantapp/listRestaurants.html', context)
+
